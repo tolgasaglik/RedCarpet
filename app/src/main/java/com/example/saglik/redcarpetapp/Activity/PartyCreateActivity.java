5b6278db1203ab72e57930a3100f8bd2cf8f66cb
tolgasaglik <tolgasaglik@gmail.com>
2017-12-18 17:38:36 +0100

    Image upload from gallery to Firebase

--- app/src/main/java/com/example/saglik/redcarpetapp/Activity/PartyCreateActivity.java
+++ app/src/main/java/com/example/saglik/redcarpetapp/Activity/PartyCreateActivity.java
@@ -2,17 +2,20 @@ package com.example.saglik.redcarpetapp.Activity;
 
 import android.content.Intent;
 import android.graphics.PorterDuff;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.saglik.redcarpetapp.Classes.Party;
 import com.example.saglik.redcarpetapp.Classes.User;
 import com.example.saglik.redcarpetapp.Database.DatabaseWriter;
 import com.example.saglik.redcarpetapp.R;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -22,28 +25,29 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class PartyCreateActivity extends AppCompatActivity {
     private static final String REQUIRED = "REQUIRED";
     User organizer = new User();
     Party party = new Party();
+    private Button imageButton;
+    private StorageReference mStorage;
 
     EditText editText1;
     EditText editText2;
     EditText editText3;
     EditText editText4;
 
-    private Button imageButton;
-    private StorageReference mStorage;
-    private static final int GALLERY_INTENT = 2;
+    private int GALLERY_INTENT = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_party_create);
         setTitle("Create a new event");
-        imageButton = findViewById(R.id.button2);
-        imageButton.getBackground().setColorFilter(R.drawable.side_nav_bar, PorterDuff.Mode.MULTIPLY);
+        Button button = findViewById(R.id.button2);
+        button.getBackground().setColorFilter(R.drawable.side_nav_bar, PorterDuff.Mode.MULTIPLY);
 
         setOrganizerName();
 
@@ -59,13 +63,26 @@ public class PartyCreateActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(Intent.ACTION_PICK);
                 intent.setType("image/*");
-                startActivityForResult(intent, GALLERY_INTENT);
+                startActivityForResult(intent,GALLERY_INTENT);
             }
         });
 
-
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==GALLERY_INTENT && resultCode==RESULT_OK){
+            Uri uri = data.getData();
+            StorageReference filePath = mStorage.child("party_images").child(uri.getLastPathSegment());
+            filePath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(PartyCreateActivity.this, "Image Uploaded", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
     private void setOrganizerName() {
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
         FirebaseUser mUser = mAuth.getCurrentUser();
